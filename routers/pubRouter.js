d312293ed1b8687f1ed79f80b238c52af722cdbf
Hizkia Jonathan Budiana <hizkia.jonathanb@gmail.com>
2025-06-16 03:52:22 +0700

    Menambahkan endpoint untuk get support entity di public

--- routers/pubRouter.js
+++ routers/pubRouter.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 //controllers
 const CuisineController = require("../controllers/cuisineController");
+const CategoryController = require("../controllers/categoryController");
 
 //public
 router.get("/cuisines", CuisineController.showPost);
 router.get("/cuisines/:id", CuisineController.showPostDetails);
+router.get("/categories", CategoryController.showCategories);
 
 module.exports = router;
