cf533cc6d2b5e6445d4b9ac04a2f0643f49848de
Hizkia Jonathan Budiana <hizkia.jonathanb@gmail.com>
2025-06-15 17:02:27 +0700

    small fix

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ const app = express();
 const router = require("./routers/router");
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.set("view engine", "ejs");
 
 app.use(router);
 
